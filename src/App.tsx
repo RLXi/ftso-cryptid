92330b41cba422d534ea69ffc8466dbd0b9a8075
Aleksi Hynönen <rolexpost@gmail.com>
2022-03-23 18:18:36 +0200

    fix text

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
   return (
     <AppShell padding={"md"}>
       <Center>
-        <h1>We much WIP</h1>
+        <h1>WIP</h1>
       </Center>
       <Body
         prevSetupStep={prevSetupStep}
