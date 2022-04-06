753b18d8068a72f1455f248ea01d413d4b04d5fc
Aleksi Hynönen <rolexpost@gmail.com>
2022-04-06 14:48:05 +0300

    clue types

--- src/types/Clues.ts
+++ src/types/Clues.ts
@@ -1,15 +1,26 @@
-import { IStructureType, IStructureColor } from "./Structure";
-
-export type TerrainTypes = "Forest" | "Mountain" | "Swamp" | "Desert" | "Water";
+type TerrainTypes = "Forest" | "Mountain" | "Swamp" | "Desert" | "Water";
+type AnimalTypes = "Bear" | "Cougar";
+type StructureTypes = "Shack" | "Stone";
+type StructureColors = "Blue" | "Green" | "White" | "Black";
 
 export type InTerrain = `In${TerrainTypes}${TerrainTypes}`;
+export type NotInTerrain = `NotIn${TerrainTypes}${TerrainTypes}`;
 
 export type WithinOne =
   | `WithinOneFrom${TerrainTypes}`
   | "WithinOneFromAnimalTerritories";
+export type NotWithinOne =
+  | `NotWithinOneFrom${TerrainTypes}`
+  | "NotWithinOneFromAnimalTerritories";
 
 export type WithinTwo =
   | `WithinTwoFrom${TerrainTypes}`
-  | `WithinTwoFrom${IStructureType}`;
+  | `WithinTwoFrom${AnimalTypes}`
+  | `WithinTwoFrom${StructureTypes}`;
+export type NotWithinTwo =
+  | `NotWithinTwoFrom${TerrainTypes}`
+  | `NotWithinTwoFrom${AnimalTypes}`
+  | `NotWithinTwoFrom${StructureTypes}`;
 
-export type WithinThree = `WithinThreeFrom${IStructureColor}`;
+export type WithinThree = `WithinThreeFrom${StructureColors}`;
+export type NotWithinThree = `NotWithinThreeFrom${StructureColors}`;
